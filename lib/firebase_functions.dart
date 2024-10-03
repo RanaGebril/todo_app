62f5143c713d5f6f2bef5d3898bd6e9952618936
Rana_Gebril <ranagebril5@gmail.com>
2024-10-03 14:32:31 +0300

    get task collection function & add function code

---
+++ lib/firebase_functions.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_app/models/task_model.dart';
+
+class FirebaseFunctions{
+  static CollectionReference<TaskModel> getTaskCollection(){
+    return FirebaseFirestore.instance.collection("Tasks").withConverter<TaskModel>(
+      fromFirestore: (snapshot, _) {
+        return TaskModel.fromJson(snapshot.data()!);
+      },
+      toFirestore: (task, _) {
+        return task.toJson();
+      },);
+  }
+
+  static Future<void> addTask(TaskModel task){
+    //create Tasks collection
+    var collection=getTaskCollection();
+
+    //create document auto id
+    var docRef=collection.doc();
+
+    task.id=docRef.id;
+
+    //add attribute to document
+    return docRef.set(task);
+  }
+}
